4bd5f01941a257349d9e48eb4ad26d1295192ba6
Ayomide <iamayomide.com@gmail.com>
2026-07-27 14:45:24 +0100

    Isolate test database from Production disasters

--- app/database.py
+++ app/database.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./tasks.db"
+if os.getenv("TESTING") == "1":
+    DATABASE_URL = "sqlite:///./test.db"
+else:
+    DATABASE_URL = "sqlite:///./tasks.db"
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
